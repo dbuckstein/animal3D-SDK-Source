eed131a64339c483c1d4f2b81292bd3646a01cdd
dbucksteincc <dbucksteincc@users.noreply.github.com>
2021-03-09 21:07:57 -0500

    Preparing new solution with OpenGL renderer project and source. ADD: solution with renderer project; all renderer source. DEL: pre-compiled GL renderer libraries.

---
+++ animal3D SDK/LAUNCH_VS_GL.bat
@@ -0,0 +1,5 @@
+:: Visual Studio Launcher (simplified plugin-only environment)
+:: By Daniel S. Buckstein
+@echo off
+set ANIMAL3D_SDK=%~dp0
+call "%ANIMAL3D_SDK%\utility\win\dev\animal3d_win_vslaunch_latest.bat" "animal3d-sdk-gl"
